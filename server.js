758e2742b115b0c47c7193e20f26b54f6bb9035b
wattamac <wattamac@hanmail.net>
2015-11-18 19:53:50 +0900

    api 추가

--- server.js
+++ server.js
@@ -6,22 +6,31 @@ var express = require('express')
 app.get('/', function (req, res) {
     console.log(__dirname);
     res.sendfile(__dirname + '/index.html');
-
 });
 
-app.get("/friends", function(req, res) {
+
+
+
+
+
+
+app.get("/api/friends", function(req, res) {
     var friends = require('./data/friends_restraunt_list');
     res.send(friends.get());
 });
 
-app.get("/restraunts", function(req, res) {
+app.get("/api/restraunts", function(req, res) {
     var restraunts = require('./data/my_restraunt_list');
     res.send(restraunts.get());
 });
 
-app.get("/restraunt/:id", function(req, res){
-    console.log(req.params.id);
-    res.send(req.params.id);
+app.get("/api/restraunt/:id", function(req, res){
+    var restraunts = require('./data/restaurantInfo');
+    var id = req.params.id;
+    console.log(id);
+    var restraunt = restraunts.get(id);
+    console.log(restraunt);
+    return res.send(restraunt);
 });
 
 server.listen(8000, function() {
